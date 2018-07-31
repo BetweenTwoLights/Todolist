22011b7b79f6f067b4b2bf77997b03d6c48dcf59
BetweenTwoLights <danielle.rdb@outlook.com>
2018-07-30 22:25:35 -0500

    Completed basic functionality and began styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,6 +1,23 @@
 // Check off todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     // toggle completion: mark-out and gray
     $(this).toggleClass("completed");
         
+});
+
+//click on X to delete to do
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13){
+        // grab new todo text from input
+        var todoText = $(this).val()
+        // create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
